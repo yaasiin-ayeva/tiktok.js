5c00aa99827fe67c9a1329b96dc4e172a94dc913
Yaasiin <ayevayaasiin@gmail.com>
2024-09-11 03:14:03 +0000

    chore : searchTag fn, added scroll param Co-authored-by: djabiridrissou <djabirtairou@gmail.com>

--- src/test.ts
+++ src/test.ts
@@ -4,12 +4,16 @@ dotenv.config();
 const { CLIENT, PASSWORD } = process.env;
 
 (async () => {
+
+
     const client = new TikTokClient();
     await client.authenticate(CLIENT, PASSWORD);
+    
     /*await client.postVideo(
         'video.mp4',
         'This is a sample description with #tags',
     ); */
+    
     const results = await client.searchTag('recre');
     console.log(results);
 })();
